b85672a5af8b2bbdbecad1c124b60ba746d7700c
Bruno DELATTRE <delattreb@gmail.com>
2018-05-25 16:54:07 +0200

    Add auth translation

---
+++ resources/lang/en/authentification.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'name' => 'Name',
+    'emailaddress' => 'E-Mail Address',
+    'password' => 'Password',
+    'confirmpassword' => 'Confirm Password',
+    'register' => 'Register',
+    'remember'=> 'Remember Me',
+    'forgot'=> 'Forgot Your Password?',
+    'login'=> 'Login',
+    'reset'=> 'Reset Password',
+    'sendpassord'=> 'Send Password Reset Link',
+];
---
+++ resources/lang/fr/authentification.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'name' => 'Nom',
+    'emailaddress' => 'Adresse E-Mail',
+    'password' => 'Mot de passe',
+    'confirmpassword' => 'Confirmation mot de  passe',
+    'register' => 'Enregistrer',
+    'remember' => 'Se souvenir de moi',
+    'forgot' => 'Mot de passe oublié?',
+    'login' => 'Se connecter',
+    'reset' => 'Reset mot de passe',
+    'sendpassord' => 'Envoyer le lien de reset mot de passe',
+];
